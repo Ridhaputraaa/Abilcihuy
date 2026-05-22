9f7549291fd127c818dfdfea9a8d53dd4e235724
Ridhaputraaa <mangeak590@gmail.com>
2026-05-22 10:33:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 AbilGacor67")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+with st.bottom:
+    st.[element_name]
